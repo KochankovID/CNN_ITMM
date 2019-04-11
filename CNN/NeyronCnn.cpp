ca469989f8de32885786312ecc1566c61d0abe73
Ilya Kochankov <fakafafakafa@mail.ru>
2019-04-11 11:38:56 +0300

    Начало

---
+++ CNN/NeyronCnn.cpp
@@ -0,0 +1,32 @@
+#include "NeyronCnn.h"
+
+
+
+Neyron—nn::Neyron—nn()
+{
+}
+
+Neyron—nn::Neyron—nn(const int & i, const int & j)
+{
+}
+
+Neyron—nn::Neyron—nn(const int **, const int & i, const int & j)
+{
+}
+
+Neyron—nn::Neyron—nn(const Neyron—nn & copy)
+{
+}
+
+void Neyron—nn::Padding()
+{
+}
+
+void Neyron—nn::Svertka()
+{
+}
+
+
+Neyron—nn::~Neyron—nn()
+{
+}
