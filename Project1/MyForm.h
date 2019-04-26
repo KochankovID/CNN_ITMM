868f0d5abb0bbb1a7e361faf8efa07f2c0d91a51
Zavulon <fakafafakafa@mail.ru>
2019-04-26 14:12:19 +0300

    Графический интерфейс

--- Project1/MyForm.h
+++ Project1/MyForm.h
@@ -21,12 +21,17 @@ namespace DrawNums {
 	bool flag;
 	Graphics^ gg;
 	Rectangle^ r;
+	Bitmap image;
 
 	public:
-		MyForm(void)
+		MyForm(void):image(300,500)
 		{
+
 			flag = false;
 			InitializeComponent();
+			recr->Image =(Image^&) image;
+			gg = Graphics::FromImage((Image^&)image);
+
 		}
 
 	protected:
@@ -57,29 +62,37 @@ namespace DrawNums {
 			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->recr = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->recr))->BeginInit();
 			this->SuspendLayout();
-
+			// 
+			// button1
+			// 
 			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::None;
 			this->button1->DialogResult = System::Windows::Forms::DialogResult::Cancel;
-			this->button1->Location = System::Drawing::Point(600, 400);
+			this->button1->Location = System::Drawing::Point(576, 406);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(100, 40);
 			this->button1->TabIndex = 0;
+			this->button1->Text = L"Распознавание";
 			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Text = "Распознавание";
 			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
-
-			this->recr->BackColor = System::Drawing::SystemColors::ControlDark;
-			this->recr->Image = Image::FromFile("E:\\CNN_ITMM\\Project1\\Image.bmp");
+			// 
+			// recr
+			// 
+			this->recr->BackColor = System::Drawing::SystemColors::Window;
+			//this->recr->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"recr.BackgroundImage")));
+			this->recr->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
 			this->recr->Location = System::Drawing::Point(10, 10);
-			this->recr->Name = L"Rect";
+			this->recr->Name = L"recr";
 			this->recr->Size = System::Drawing::Size(300, 500);
 			this->recr->TabIndex = 0;
+			this->recr->TabStop = false;
 			this->recr->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button1_Click);
 			this->recr->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button2_Click);
 			this->recr->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::button3_Click);
-			this->recr->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
-
+			// 
+			// MyForm
+			// 
 			this->AcceptButton = this->button1;
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
@@ -90,11 +103,12 @@ namespace DrawNums {
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->recr);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
-			this->Name = L"Form1";
+			this->Name = L"MyForm";
 			this->Text = L"Form1";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->recr))->EndInit();
 			this->ResumeLayout(false);
-			
-		} 
+
+		}
 
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
@@ -102,11 +116,7 @@ namespace DrawNums {
 	}
 	private: System::Void button2_Click(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
 		if (flag) {
-			Bitmap b(recr->Width, recr->Height);
-			gg = Graphics::FromImage((Image^&)b);
-			gg->DrawEllipse(System::Drawing::Pens::Black, e->X, e->Y, 10, 10);
-			gg->FillEllipse(System::Drawing::Brushes::Black, e->X, e->Y, 10, 10);
-			this->recr->Image = (Image^&)b;
+			gg->FillEllipse(System::Drawing::Brushes::Black, e->X, e->Y,10,10);
 		}
 	}
 	private: System::Void button3_Click(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
@@ -114,13 +124,14 @@ namespace DrawNums {
 	}
 
 	private: System::Void button4_Click(System::Object^  sender, System::EventArgs^  e) {
-		Bitmap b (recr->Image,recr->Width, recr->Height);
+		Bitmap o(recr->Width, recr->Height, gg);
+		o.Save("E:\\CNN_ITMM\\Project1\\Image.bmp", );
 		long int a4 = 0;
 		for (int i = 0; i < recr->Height; i++)
 		{
 			for (int h = 0; h < recr->Width; h++)
 			{
-				if (((Bitmap^)recr->Image)->GetPixel(h, i) == Color::Black)
+				if (o.GetPixel(h, i) == Color::Black)
 					a4++;
 			}
 		}
